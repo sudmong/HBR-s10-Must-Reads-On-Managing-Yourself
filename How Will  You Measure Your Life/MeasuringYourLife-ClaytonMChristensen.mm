7c2748db5c16241f4bebf41a38dab62bdaef4876
Sudhir Mongia <sudhir.mongia@gmail.com>
2017-12-25 14:33:42 +0530

    How will you measure your life?
    Mind maps of learning from Chapter 1  from the book. Raw freemind file and the exported png  file of the same.

---
+++ How Will  You Measure Your Life/MeasuringYourLife-ClaytonMChristensen.mm
@@ -0,0 +1,70 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1514186304958" ID="ID_796225793" MODIFIED="1514191273843" TEXT="How will you Measure Your Life : Clayton M. Christensen">
+<font BOLD="true" ITALIC="true" NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514186430847" ID="ID_377122635" MODIFIED="1514187119341" POSITION="right" TEXT="Instead of telling people what to think, teach them how to think, they will be able to find answers to their questions">
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1514186569000" ID="ID_534302365" MODIFIED="1514187145047" POSITION="left" TEXT="Three Questions">
+<font BOLD="true" NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514186584601" ID="ID_774284900" MODIFIED="1514187180859" TEXT="How can I be sure that I&apos;ll be happy in My career?">
+<font NAME="Segoe UI" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1514186609387" ID="ID_1730911096" MODIFIED="1514187231341" TEXT="How can i be sure that my relationships with my spouse and family will become an ensuring source of happiness?">
+<font NAME="Segoe UI" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1514186656771" ID="ID_45427123" MODIFIED="1514187243595" TEXT="How can i live my life with integrity?">
+<font NAME="Segoe UI" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1514186703644" ID="ID_1774830902" MODIFIED="1514187153992" POSITION="right" TEXT="Money vs Satisfaction">
+<font BOLD="true" NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514186716311" ID="ID_794358086" MODIFIED="1514187119319" TEXT="Frederick Herzberg Asserts that the powerful motivator in our livesis not the money; its the oppotunity to learn, grow in responsibilities, contribute to others, and be recognized for achievements">
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1514186830243" ID="ID_198988802" MODIFIED="1514187147925" POSITION="left" TEXT="About Management">
+<font BOLD="true" NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514186845651" ID="ID_1743495947" MODIFIED="1514187119303" TEXT="Management is the most noble of professions if practiced well. No other occupation offers as many ways to help others learn, grow, take responsibility and be recognized for achievement, and contribute to success of a team">
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1514186971493" ID="ID_471623720" MODIFIED="1514187151511" POSITION="right" TEXT="How to do it">
+<font BOLD="true" NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514186987532" ID="ID_1375736460" MODIFIED="1514187119303" TEXT="Create a Strategy for your life">
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514191460600" ID="ID_892947188" MODIFIED="1514191505426" TEXT="Family life and career life"/>
+<node CREATED="1514191505639" ID="ID_993201422" MODIFIED="1514191622381" TEXT="Finding the purpose, without the purpose , life can become hollow"/>
+<node CREATED="1514191624721" ID="ID_52116108" MODIFIED="1514191724452" TEXT="If you are not guided by a clear sense of purpose, you are likely to fritter away your time and energy on obtaining the most tangible, short-term signs on achievement, not what is really important to you"/>
+</node>
+<node CREATED="1514186998692" ID="ID_1887155727" MODIFIED="1514187119303" TEXT="Allocate your Resources">
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514191337507" ID="ID_1900076760" MODIFIED="1514191372467" TEXT="Your decisions about allocating your personal time, energy and talent ultimately shape your life&apos;s strategy"/>
+</node>
+<node CREATED="1514187009279" ID="ID_1481228010" MODIFIED="1514187119303" TEXT="Create a Culture">
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514191840742" ID="ID_574603079" MODIFIED="1514191860434" TEXT="This is relates to second question about relationship with family"/>
+<node CREATED="1514191863090" ID="ID_829483551" MODIFIED="1514191943405" TEXT="You&apos;ll need to setup a culture early on to make your child have high self esteem and ability to solve big problems"/>
+</node>
+<node CREATED="1514187018458" ID="ID_943984149" MODIFIED="1514187119303" TEXT="Avoid the &quot;Marginal Costs&quot; mistake">
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514191026781" ID="ID_641795093" MODIFIED="1514191064742" TEXT="This is related to third question on living life with integrity"/>
+<node CREATED="1514191045857" ID="ID_750668646" MODIFIED="1514191196849" TEXT="Marginal cost of doing something wrong &quot;just this once&quot; always seems low."/>
+<node CREATED="1514191132417" ID="ID_85033782" MODIFIED="1514191240411" TEXT="It is easier to hold to your principles 100% of the time than it is to hold them 98% of the time"/>
+</node>
+<node CREATED="1514187030762" ID="ID_1403474728" MODIFIED="1514187119303" TEXT="Remember the Importance of Humility">
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514190573527" ID="ID_1016744098" MODIFIED="1514190821275" TEXT="Generally, you&apos;ll be humble only if you feel good about yourself"/>
+<node CREATED="1514190877568" ID="ID_547244881" MODIFIED="1514190927397" TEXT="Humble people usually have high self esteem"/>
+<node CREATED="1514190929481" ID="ID_1027281976" MODIFIED="1514190993235" TEXT="Humility is was defined not by self deprecating behavior or attitudes BUT by the esteem with which you regard others"/>
+</node>
+<node CREATED="1514187047680" ID="ID_4530837" MODIFIED="1514187119303" TEXT="Choose the right yardstick">
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1514190492582" ID="ID_392544076" MODIFIED="1514190552273" TEXT="Don&apos;t worry about the level of personal prominence you have achieved; worry about the individuals you have helped become better people"/>
+</node>
+</node>
+</node>
+</map>
